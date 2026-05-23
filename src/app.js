ce16527b00d7444b995d0f624640808b8bac70e1
Shivam Deolankar <shivam.deolankar@gmail.com>
2026-05-23 20:06:08 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/health",(req, res)=>{
+    return res.status(200).json({
+        status: "ok",
+    });
+});
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/db.js";
+import { PORT } from "./constants.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((err)=>{
+  console.log("MongoDB connection failed : ",error)
+});
