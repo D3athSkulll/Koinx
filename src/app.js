a3e7357c1839c502126536fc36097c026b224585
Shivam Deolankar <shivam.deolankar@gmail.com>
2026-05-24 19:31:34 +0530

    chore: modify root endpoint for easy testing

--- src/app.js
+++ src/app.js
@@ -13,9 +13,32 @@ app.use(
   }),
 );
 
+// app.get("/", (req, res) => {
+//   res.send("KoinX Reconciliation API Running");
+// });
+
 app.get("/", (req, res) => {
-  res.send("KoinX Reconciliation API Running");
+  res.status(200).json({
+    success: true,
+    message: "KoinX Reconciliation Engine API Running",
+    description: "Backend API for ingesting, reconciling, and reporting crypto transactions.",
+    procedure: "Test ingest and reconcile to get runId, then test Report and Download endpoints.",
+    endpoints: {
+      ingest: "POST /api/ingest",
+      reconcile: "POST /api/reconcile",
+      fullReport: "GET /api/report/:runId",
+      summaryReport: "GET /api/report/:runId/summary",
+      unmatchedTransactions: "GET /api/report/:runId/unmatched",
+      downloadCSVReport: "GET /api/report/:runId/download",
+    },
+
+    testing: {
+      downloadMethod: "Auto Download CSV report for download endopoint, through browser",
+      recommendedTool: "Postman for other endpoints",
+    },
+  });
 });
+
 
 app.get("/health", (req, res) => {
   return res.status(200).json({
